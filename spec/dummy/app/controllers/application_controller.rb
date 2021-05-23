eb7e87a666252db8fd467a08b38f8616a8ce4ec8
Dima Samodurov <dimasamodurov@gmail.com>
2021-05-23 11:59:23 -0500

    Add link to demo route to welcome message.

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    render plain: 'Hello from Dummy app!'
+    render html: <<~HTML.html_safe
+      <p>Hello from Dummy app!</p> 
+      <p>You might want to visit #{helpers.link_to('/mr_video', '/mr_video')} path to see engine in action.</p>
+    HTML
   end
 end
